20a6b269a90bbf86d32b671865bb5b7ed93e90ad
Melissa DeLucchi <delucchi@andrew.cmu.edu>
2026-07-13 10:54:23 -0400

    Silly ruff!

--- tests/lsdb_rubin/test_rsp_tests.py
+++ tests/lsdb_rubin/test_rsp_tests.py
@@ -1,12 +1,12 @@
-from pathlib import Path
-
 from lsdb_rubin.rsp_tests.critical_functions import critical_functions
 from lsdb_rubin.rsp_tests.random_access import random_access
 
 
 def test_critical_functions():
+    """Self-explanatory."""
     critical_functions()
 
 
 def test_random_access(test_data_dir):
+    """Self-explanatory."""
     random_access(test_data_dir / "mock_dp1_1000")
